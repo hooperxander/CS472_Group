d89ef53c2d0606ec0ba0d21f464b5e8f73f755d9
Xander Hooper <hooperxander@gmail.com>
2020-04-01 10:56:36 -0600

    made a loop to average a few test because cross_val from np is kinda hard to use

--- Example.py
+++ Example.py
@@ -1,8 +1,25 @@
 import numpy as np
+from sklearn.model_selection import cross_val_score
 from sklearn.neural_network import MLPRegressor
 from sklearn.neighbors import KNeighborsRegressor
 
 from CS472_Group.DataPreprocessor import DataPreprocessor
+
+
+class BaseLine:
+    def fit(self):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def score(self, X, y):
+        x = np.array(X)
+        error = y - x[:, 0]
+        return np.sum(error ** 2)
+
+    def get_params(self, deep=False):
+        return []
 
 preprocessor = DataPreprocessor()
 X_train, X_test, y_train, y_test = preprocessor.get_train_test_data()
@@ -18,6 +35,11 @@ knn.fit(X_train, y_train)
 print(knn.predict(X_test))
 
 # BASELINE
-x = np.array(X_test)
-error = y_test-x[:,0]
-print("Baseline SSE: " + str(np.sum(error**2)))
+total = 0
+for index in range(10):
+    X_train, X_test, y_train, y_test = preprocessor.get_train_test_data()
+    x = np.array(X_test)
+    error = y_test - x[:, 0]
+    total = total + np.sum(error ** 2)
+print("average SSE" + str(total/10))
+
