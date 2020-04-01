899f8a8826d886ddcede4509fccf2ea408aa7d58
Xander Hooper <hooperxander@gmail.com>
2020-04-01 10:40:37 -0600

    basic baseline SSE, need to add n-fold CV

--- DataPreprocessor.py
+++ DataPreprocessor.py
@@ -1,22 +1,23 @@
-import torch
+#import torch
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
 class DataPreprocessor:
-  def get_train_test_data(self):
-    data = pd.read_csv('data.csv')
-    data_list = torch.tensor(data.values).tolist()
+    def get_train_test_data(self):
+        data = pd.read_csv('data.csv')
+        data_list = data.values
+        #data_list = torch.tensor(data.values).tolist()
 
-    X = []
-    y = []
-    for i in range(len(data_list)):
-      row = []
-      for j in range(len(data_list[i])):
-        if j == len(data_list[i])-1:
-          y.append(data_list[i][j])
-        else:
-          row.append(data_list[i][j])
-      X.append(row)
+        X = []
+        y = []
+        for i in range(len(data_list)):
+            row = []
+            for j in range(len(data_list[i])):
+                if j == len(data_list[i]) - 1:
+                    y.append(data_list[i][j])
+                else:                    row.append(data_list[i][j])
+            X.append(row)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, train_size=0.75)
-    return X_train, X_test, y_train, y_test
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, train_size=0.75)
+        return X_train, X_test, y_train, y_test
--- Example.py
+++ Example.py
@@ -1,11 +1,14 @@
+import numpy as np
 from sklearn.neural_network import MLPRegressor
 from sklearn.neighbors import KNeighborsRegressor
+
+from CS472_Group.DataPreprocessor import DataPreprocessor
 
 preprocessor = DataPreprocessor()
 X_train, X_test, y_train, y_test = preprocessor.get_train_test_data()
 
 clf = MLPRegressor(solver='lbfgs', alpha=1e-5,
-                    hidden_layer_sizes=(10, 10, 10), random_state=1)
+                   hidden_layer_sizes=(10, 10, 10), random_state=1)
 clf.fit(X_train, y_train)
 print(clf.predict(X_test))
 
@@ -13,3 +16,8 @@ knn = KNeighborsRegressor(n_neighbors=3)
 knn.fit(X_train, y_train)
 
 print(knn.predict(X_test))
+
+# BASELINE
+x = np.array(X_test)
+error = y_test-x[:,0]
+print("Baseline SSE: " + str(np.sum(error**2)))
